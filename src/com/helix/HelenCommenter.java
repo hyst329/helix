d105d502bf1eb7d6057e62826683edbfc8b9cc17
hyst329 <trolleybus.1329@gmail.com>
2016-03-21 10:08:39 +0300

    added HelenCommenter

---
+++ src/com/helix/HelenCommenter.java
@@ -0,0 +1,40 @@
+package com.helix;
+
+/**
+ * Created by main on 21.03.2016.
+ */
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class HelenCommenter implements Commenter {
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "#";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return "";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
